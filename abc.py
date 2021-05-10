df8dac69974db6734c985459f8490161a9d537fa
karthikkumar21 <83963759+karthikkumar21@users.noreply.github.com>
2021-05-10 19:25:08 +0530

    Created the area of the circle and file extension
    this file shows the output of pie

---
+++ abc.py
@@ -0,0 +1,7 @@
+r=float(input("Enter the radius:"))
+area=22/7*r*r
+print("The area of circle is",area)
+File_name = input("Enter the file name : ")
+print("The file name is :",File_name)
+File_ex = File_name.split(".")
+print("The extension of the file is : " + repr(File_ex[-1]))
